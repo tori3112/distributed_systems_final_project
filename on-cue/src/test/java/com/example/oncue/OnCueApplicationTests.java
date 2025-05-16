3a3cd470f7dfe08a91adbd2fd5c733fb57979a61
nadiaazizi <nadianajib1102@gmail.com>
2025-05-16 11:50:18 +0200

    initial setup for ticket supplier

---
+++ on-cue/src/test/java/com/example/oncue/OnCueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oncue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnCueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
