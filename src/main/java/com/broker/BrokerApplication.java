36d5d30a8d3d2e5f7bfd6df1209396580350016b
Belgin ayvat <begoayvat@hotmail.com>
2025-05-24 00:57:00 +0200

    added cors functionality for the frontend to access the backend

--- src/main/java/com/broker/BrokerApplication.java
+++ src/main/java/com/broker/BrokerApplication.java
@@ -2,8 +2,11 @@ package com.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @SpringBootApplication
@@ -12,5 +15,13 @@ public class BrokerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BrokerApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
