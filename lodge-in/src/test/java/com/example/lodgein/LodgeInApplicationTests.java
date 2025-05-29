0156e02fa504ea7ee91b2e601c52c462a205f8f6
nadiaazizi <nadianajib1102@gmail.com>
2025-05-29 21:10:39 +0200

    Created lodge in

---
+++ lodge-in/src/test/java/com/example/lodgein/LodgeInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lodgein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgeInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
