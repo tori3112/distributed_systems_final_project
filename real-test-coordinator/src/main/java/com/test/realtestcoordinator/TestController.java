b6ad52730a858618ba3a1f2efadff23c20d884c9
nadiaazizi <nadianajib1102@gmail.com>
2025-06-09 02:21:40 +0200

    Created new test coordinator

---
+++ real-test-coordinator/src/main/java/com/test/realtestcoordinator/TestController.java
@@ -0,0 +1,43 @@
+package com.test.realtestcoordinator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @PostMapping("/initiate_2pc")
+    private boolean callPhase() {
+        //init dummy data
+        Order order = new Order();
+        order.setId(1);
+        order.setAddress("Melrose Avenue");
+        order.setAccom_id(2);
+        order.setPaid(false);
+        order.setPackage_id(1);
+        order.setTicket_id(3);
+        try {
+
+            log.info("success , bar low");
+            boolean isAccommSuccess = callServices("http://localhost:8080/prepare_accomm", order);
+
+            return isAccommSuccess;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    private boolean callServices(String url, Order order) {
+        ResponseEntity<String> response = restTemplate.postForEntity(url, order, String.class);
+        return response.getStatusCode().is2xxSuccessful();
+    }
+}
---
+++ real-test-coordinator/src/test/java/com/test/realtestcoordinator/RealTestCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.realtestcoordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTestCoordinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
