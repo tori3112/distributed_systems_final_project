5f3ba526a67177c642ca1d1c0b2d79de4644897f
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-06-14 13:11:16 +0200

    fixed eslint

--- frontend/src/components/LoginButtons.js
+++ frontend/src/components/LoginButtons.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import Profile from "./Profile";
 
 const AuthButtons = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
     <div>
