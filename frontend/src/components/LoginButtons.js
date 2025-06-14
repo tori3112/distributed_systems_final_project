d65e368b389f82a466af3112362688936299b338
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-06-14 13:06:21 +0200

    getting user metadata

--- frontend/src/components/LoginButtons.js
+++ frontend/src/components/LoginButtons.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import Profile from "./Profile";
 
 const AuthButtons = () => {
-  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
     <div>
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  const [ userMetaData, setUserMetaData ] = useState(null);
+
+  console.log('user is ', user);
+
+  useEffect(() => {
+    const getUserMetaData = async () => {
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: `${process.env.AUTH0_IDENTIFIER}`,
+            scope: 'read:users_app_metadata'
+          },
+        });
+
+        const userDetailsByURL = `${process.env.AUTH0_IDENTIFIER}/users/${user.sub}`;
+        const metaDataResponse = await fetch(userDetailsByURL, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          }
+        });
+
+        const { user_metadata } = await metaDataResponse.json();
+        setUserMetaData(user_metadata);
+      } catch (error) {
+        console.error('Error when getting user metadata: ', error);
+      }
+    };
+    getUserMetaData();
+  }, [getAccessTokenSilently, user?.sub]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -11,10 +41,10 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div className="flex items-center space-x-4">
-        <img src={user.picture} alt={user.name} className="w-10 h-10 rounded-full" />
+        <img src={userMetaData.picture} alt={userMetaData.name} className="w-10 h-10 rounded-full" />
         <div>
-          <h2 className="text-lg font-semibold">{user.name}</h2>
-          <p className="text-sm text-fuchsia-600">{user.email}</p>
+          <h2 className="text-lg font-semibold">{userMetaData.name}</h2>
+          <p className="text-sm text-fuchsia-600">{userMetaData.email}</p>
         </div>
       </div>
     )
