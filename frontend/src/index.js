32b5608f027f6de715fcf32923cd2e90ef108126
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-06-14 13:38:24 +0200

    index fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,8 +13,7 @@ root.render(
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
-      audience: `${process.env.AUTH0_IDENTIFIER}`,
-      scope: "read:current_user update:current_user_metadata"
+      audience: `${process.env.AUTH0_AUDIENCE}`,
     }}
     >
     <App />
