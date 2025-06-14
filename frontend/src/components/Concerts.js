69bf9e88513e215bc7d12c3ff7af1c92b4da7a55
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-06-14 16:44:05 +0200

    added quanit

--- frontend/src/components/Concerts.js
+++ frontend/src/components/Concerts.js
@@ -50,6 +50,16 @@ function Concerts({ onSelectConcert }) {
                   {concert.venue}
                   </p>
                   <p className='mb-2 font-normal text-gray-700'>Ticket Type: {concert.ticketType}</p>
+                  <div class="inline-block relative w-64">
+                    <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                      <option>Really long option that will likely overlap the chevron</option>
+                      <option>Option 2</option>
+                      <option>Option 3</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                    </div>
+                  </div>
                   <button
                     onClick={() => handleGetTicket(concert)}
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-fuchsia-700 rounded-lg hover:bg-fuchsia-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-300">
